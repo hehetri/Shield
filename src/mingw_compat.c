557857114dc499ffc19170d0276e717c2672944f
Lucas Moreira <hehetri@gmail.com>
2026-02-03 13:37:09 -0800

    Add __mingw_vprintf shim for CRT-less build

--- src/mingw_compat.c
+++ src/mingw_compat.c
@@ -1,12 +1,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+int __mingw_vprintf(const char *format, va_list args) {
+    return _vprintf(format, args);
+}
+
 int __mingw_printf(const char *format, ...) {
     int result;
     va_list args;
 
     va_start(args, format);
-    result = vprintf(format, args);
+    result = __mingw_vprintf(format, args);
     va_end(args);
 
     return result;
