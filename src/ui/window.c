b5fc446a6db29800a536bcad860af6ef3d58d58a
Lucas Moreira <hehetri@gmail.com>
2026-02-10 06:47:01 -0800

    Add camera zoom hotkeys and route input via custom WndProc

--- src/ui/window.c
+++ src/ui/window.c
@@ -5,13 +5,23 @@
 // #define g_DerivedMouseX() // [g_GameClassHandle]+0x5D4
 // #define g_DerivedMouseY() // [g_GameClassHandle]+0x5DC
 
+typedef LRESULT (CALLBACK *FP_BOUT_WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+
+static LRESULT CALLBACK Window_WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    if (message == WM_KEYDOWN || message == WM_SYSKEYDOWN)
+        CUI_HandleGlobalHotkeys(wParam);
+
+    FP_BOUT_WNDPROC boutWndProc = (FP_BOUT_WNDPROC)0x403B0C;
+    return boutWndProc(hWnd, message, wParam, lParam);
+}
+
 int Window_Init(HINSTANCE instance, int cmdShow, HWND* pWindow, HACCEL* pAccel) {
     // create window class
     HICON icon = LoadIconA(instance, 107);
     WNDCLASSEX wndclass;
     wndclass.cbSize        = sizeof(wndclass);
     wndclass.lpszClassName = WINDOW_TITLE;
-    wndclass.lpfnWndProc   = (WNDPROC)0x403B0C;
+    wndclass.lpfnWndProc   = Window_WndProc;
     wndclass.style         = 0; // no style flags
     wndclass.hInstance     = instance;
     wndclass.hIcon         = icon;
